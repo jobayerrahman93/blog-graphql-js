fdbcf9871ee7ac3506c27b48615dab9f63ce4828
jobayercse71 <rahmanjobayer71@gmail.com>
2023-01-14 18:54:53 +0600

    addd quotes property in users query

--- src/graphql/resolvers/userResolver.js
+++ src/graphql/resolvers/userResolver.js
@@ -1,3 +1,4 @@
+import Quote from "../../mongoose/model/Quotes.js";
 import User from "../../mongoose/model/User.js";
 
 
@@ -6,6 +7,12 @@ export const resolvers = {
     Query: {
         users: async() => await User.find({}),
         getUserById:async(_,args)=>await User.findOne({_id:args._id}),
+      },
+      User:{
+        quotes:async(parent)=> {
+          const quote = await Quote.find({by:parent._id})
+         return quote;
+        }
       },
   
   };
